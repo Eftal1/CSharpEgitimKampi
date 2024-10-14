1f4a6d7d1aad2b8f47e0dc3a815eafc5a21ad48d
Eftal Yildiz <eftalyildiz88@gmail.com>
2024-10-14 07:50:57 +0300

    4.Ders Eklendi.

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -10,6 +10,161 @@ namespace _04_Loops
     {
         static void Main(string[] args)
         {
+
+            #region forDongusu
+            //Döngüler
+            //for(x;y;z)
+            //X: başlangıç
+            //y: bitiş
+            //z: artış-azalış
+
+            //int i; 
+
+            //for(i=1; i<=5; i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+            //for (int i = 1; i <= 20;  i++)
+            //{
+            //    Console.Write(i);
+            //}
+
+            //for (int i = 3; i <= 50; i +=3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Lütfen Ekrana Yazılmasını İstediğiniz Adeti Giriniz: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= finishValue; i++)
+            //{
+            //    Console.WriteLine("Yaşasın Cumhuriyet");
+            //}
+
+
+
+
+
+
+            #endregion
+
+            #region forDongusuIleKararYapilari
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i %5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int totalValue = 0;
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    totalValue += i;
+            //}
+            //Console.WriteLine(totalValue);
+
+            //int totalValue = 0;
+
+            //for(int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+            //Console.WriteLine("--------");
+            //Console.WriteLine(totalValue);
+
+            //int count = 0;
+            //for (int i = 1; i <= 60; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //c# Eğtim Kitabı sf:116 örneği Vize Sorusu olabilir
+
+            //int bacterium = 1;
+
+            //for (int i = 1; i <=24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + "Saat Sonunda: " + bacterium);
+            //}
+
+
+
+
+            #endregion
+
+            #region WhileDongusu
+            //şart sağlandığı müddetçe
+            //While(Şart)
+            //{
+            //İşlemler
+            //}
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba DÖngüler");
+            //    i++;
+            //}
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+
+            //while (i <= 10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            #region OrnekSinavSorusu
+            // Klavyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan kodu yazınız
+
+            Console.Write("Sayıyı Giriniz: ");
+            int number = int.Parse(Console.ReadLine());
+            int ones, tens, hundreds;
+            int sum;
+
+            ones = number % 10;
+            tens = (number % 100) / 10;
+            hundreds = number / 100;
+            
+
+            Console.WriteLine(ones + "-"+ tens + "-" + hundreds);
+            sum =ones + tens + hundreds;
+            Console.WriteLine(sum);
+
+
+            
+            #endregion
+
+            Console.Read();
+
         }
     }
 }
